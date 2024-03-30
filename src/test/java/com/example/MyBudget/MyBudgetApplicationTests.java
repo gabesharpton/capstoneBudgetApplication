e6fcc05bd635f6ed5a31683559eac2662c0a36c7
Gabe Sharpton <gabriel.sharpton@gmail.com>
2024-03-30 16:13:22 -0500

    Initial project with basic homescreen and about page navigation.

---
+++ src/test/java/com/example/MyBudget/MyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
